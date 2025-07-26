71167623312cee712c57db452254340cfb71b3ad
Logan Frederick <loganfonline@gmail.com>
2025-07-26 17:26:58 -0400

    migrate to lua for neovim configuration

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
 dotfiles
 --------
 
-This repo is intended to help me keep dotfiles synchronized across all the
-computers I use on a regular basis. Uploading them publicly will also force me
-to keep them reasonably clean and well-commented.
+This repo contains my configuration dotfiles. I may also add general new computer provisioning checklists and scripts.
 
-Current contents:
+I do contract work and clients often want me to use their equipment, so my goal here is to contain everything I need to get 80% comfy with a new Windows, Linux, or MacOS machine in 90 minutes or less.
 
- - vimrc
+### current contents:
+
+- `.vimrc` (deprecated)
+- `nvim/`
+
+### current approach:
+
+I spend a lot of time in Vim/NeoVim. I don't feel compelled to do _everything_ in it, but it is definitely my base-camp.
+
+At home I use the `vimwiki` and `vim-zettel` plugins to organize the inputs and outputs of my digital life.
+
+At work I use a more brutalist system that revolves around creating a batch of text files for each new gig.
+
+Key principles:
+
+1. no personal files on work computers
+2. clear separation of confidential and non-confidential files
+3. all configuration lives in one place that is easy to back-up
+4. it should be quick to move most text to and from vim
+5. hotkeys should be the same or similar regardless of OS
---
+++ nvim/init.lua
@@ -0,0 +1,126 @@
+-- symlink to install
+--  e.g. ln -s ~/source/dotfiles/nvim ~/.config/nvim
+
+
+local utils = require('utils')
+
+-- load plugins, including:
+--
+--  - vim-sensible      defaults we can all agree on
+--  - vim-sleuth        heuristically set buffer options
+--  - vim-vinegar       combine with netrw for a delicious salad dressing
+--  - vim-surround      change surrounding tags with ease
+--  - vim-fugitive      a git wrapper so awesome it should be illegal
+--  - vim-speeddating   increment/decrement dates, times, etc
+--
+--  - jedi-vim          python
+--  - vim-flake8        python linting
+--  - fzf.vim           fuzzy-finding
+--
+--  - vimwiki           wiki editing
+--  - vim-zettel        zettelkasten
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    'tpope/vim-sensible',
+    'tpope/vim-sleuth',
+    'tpope/vim-vinegar',
+    'tpope/vim-surround',
+    'tpope/vim-fugitive',
+    'tpope/vim-speeddating',
+    'davidhalter/jedi-vim',
+    'nvie/vim-flake8',
+    { 'junegunn/fzf', build = ':call fzf#install()' },
+    'junegunn/fzf.vim',
+    { 'vimwiki/vimwiki',
+      lazy = false,
+      cond = utils.is_personal_computer(),
+  },
+  {
+      'michal-h21/vim-zettel',
+      lazy = false,
+      cond = utils.is_personal_computer(),
+      init = function()
+        require('vimwiki').setup()
+      end,
+      dependencies = { 'vimwiki/vimwiki' }
+  },
+})
+
+
+vim.opt.list = true
+vim.opt.listchars = "tab:>-,trail:$,space:·,nbsp:┚,precedes:→"
+vim.opt.laststatus = 2
+
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.smartcase = true
+vim.opt.showcmd = true
+
+vim.g.filetype_md = 'pandoc'
+vim.opt.linebreak = true
+vim.opt.number = true
+vim.opt.splitbelow = true
+
+local function SlickOpen(key, filename, append)
+
+  if vim.tbl_contains({'j', 'h', 't'}, key) then
+    error(string.format('E999: key "%s" is reserved and cannot be used here', key))
+  end
+
+  local goto_end = (not append) and ' ' or ' + '
+  local options = { noremap = true, silent = true }
+
+  local function open_with(win_cmd)
+    local function f() vim.cmd(win_cmd .. goto_end .. vim.fn.fnameescape(filename)) end
+    return f
+  end
+
+  vim.keymap.set('n', '<Leader>' .. key .. key, open_with('edit'), options)
+  vim.keymap.set('n', '<Leader>' .. key .. 'j', open_with('split'), options)
+  vim.keymap.set('n', '<Leader>' .. key .. 'h', open_with('vsplit'), options)
+  vim.keymap.set('n', '<Leader>' .. key .. 't', open_with('tabedit'), options)
+end
+
+if utils.is_external_computer() then
+
+    local function NotesFiles(file)
+        return '~/Source/notes/' .. file
+    end
+
+    local function DotFiles(file)
+        return '~/Source/dotfiles/' .. file
+    end
+
+    SlickOpen('a', NotesFiles('acronyms.tsv'))
+    SlickOpen('i', NotesFiles('master_control_trainings.md'))
+    SlickOpen('p', NotesFiles('win_provisioning.md'))
+    SlickOpen('q', NotesFiles('qms.tsv'))
+    SlickOpen('s', 'C:\\ProgramData\\sioyek')
+    SlickOpen('v', DotFiles('init.vim'))
+    SlickOpen('f', NotesFiles('Microsoft.PowerShell_profile.ps1'))
+    SlickAppend('?', NotesFiles('howto.md'), true)
+    SlickAppend('l', NotesFiles('links_and_learnings.md'), true)
+else
+    SlickOpen('p', utils.build_path('VIMWIKI_PATH', '~/vimwiki/zettelkasten/250726-1604-new_computer.md'))
+    SlickOpen('v', vim.fn.expand('$MYVIMRC'))
+    --SlickOpener('f', NotesFiles('Microsoft.PowerShell_profile.ps1'))
+    SlickOpen('?', utils.build_path('VIMWIKI_PATH', 'zettelkasten/230401-1055-howto.md'), true)
+    --SlickAppend("l", NotesFiles("links_and_learnings.md"))
+
+end
+
+
+vim.g.mapleader = '\\'
+vim.api.nvim_set_keymap('n', '<Leader><Leader>', 'nohl', { noremap = true, silent = true })
+vim.cmd([[colorscheme retrobox]])
